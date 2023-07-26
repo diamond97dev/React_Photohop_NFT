400d944d4ce8b15d552ee88d188c6d5febf0202a
Diamond <diamond97dev@gmail.com>
2023-07-26 03:27:41 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+//import FileUploadComponent from './components/fileupload/fileupload';
+import Courses from './components/fileupload/fileupload';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+      <Courses />
+    </>
+  );
+}
+
+export default App;
